bf85d566dcd49323d1d8d62b87f2d8d707bc6864
madhuy <madhuy@yupptv.com>
2025-04-15 15:02:02 +0530

    angular upgraded to 19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+   templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+  imports: [
+    RouterOutlet,
+    FooterComponent,
+    HomeComponent,
+    HeaderComponent
+  ],
 })
 export class AppComponent {
-  title = 'synergybuildworks';
+  title = 'synergybuildworksv1';
 }
